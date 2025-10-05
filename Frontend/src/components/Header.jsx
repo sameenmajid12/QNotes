3200a38c8c777ef833ab4e1c4c79cb9212410e9b
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-10-04 23:12:47 -0400

    Create header component

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "../styles/header.css";
+function Header({ onLoginClick, onSignupClick, type }) {
+  return (
+    <header
+      className={`${type === "dashboard" ? "header-dashboard" : "header"}`}
+    >
+      <div className="logo-placeholder">Q Notes</div>
+      <div className="auth-buttons">
+        <button className="primary-btn login-btn" onClick={onLoginClick}>
+          Login
+        </button>
+        <button className="secondary-btn signup-btn" onClick={onSignupClick}>
+          Sign Up
+        </button>
+      </div>
+    </header>
+  );
+}
+export default Header;
